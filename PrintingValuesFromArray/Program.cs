742c618f43ac9fc9f4aedc828b101e24d8712b5e
Maryna Romaniuk <luv_this_world@hotmail.com>
2015-08-19 12:15:44 +0300

    initial commit

---
+++ PrintingValuesFromArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingValuesFromArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] myArray = new string[5];
+            Console.WriteLine("Enter 5 integers");
+            for(var i = 0; i<5; i++)
+            {
+                var input = Console.ReadLine();
+                myArray[i] = input;
+            }
+            foreach(string i in myArray)
+            {
+                Console.WriteLine(i);
+            }
+            
+
+        }
+    }
+}
